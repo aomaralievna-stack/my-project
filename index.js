eda649d0fcb88c4fcee603f8954620afb6426e82
aomaralievna-stack <aomaralievna@gmail.com>
2026-09-20 00:27:12 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// alert("hello word");
+// confirm("hello word");
+// prompt("what is you name");
+
+// console.log("hello word");
+
+// let-
+// const
+// var
+
+
+// let name="Айгерим"
+// alert(name);
+
+// let name=prompt("what is you name");
+// alert(name);
+
+
+prompt("Салам сиздин атыныз ким?");
+prompt("Сиздин жашыныз канчада?");
+
